1523e2d9833793f2675cf1e3a17c9c0afc4680dc
9b84n87ssk-create <9b84n87ssk@privaterelay.appleid.com>
2026-08-11 13:51:56 +0800

    Perf: externalize/minify CSS, minify HTML, add service worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+﻿const CACHE_NAME = 'site-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate' || (event.request.headers.get('accept') && event.request.headers.get('accept').includes('text/html'))) {
+    event.respondWith(fetch(event.request).then(r => { caches.open(CACHE_NAME).then(c=>c.put(event.request, r.clone())); return r; }).catch(()=>caches.match('/index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request).then(fetchRes=>{ caches.open(CACHE_NAME).then(c=>c.put(event.request, fetchRes.clone())); return fetchRes;})));
+});
